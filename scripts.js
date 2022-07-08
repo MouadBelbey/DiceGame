5fbccea971beb90b3fe18ef522d447c9979d1fc0
Mouad Belbey <104021030+MouadBelbey@users.noreply.github.com>
2022-07-08 15:47:24 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var num1 = "images/dice"+randomNumber1+".png";
+var num2 = "images/dice"+randomNumber2+".png";
+
+
+
+document.querySelector(".img1").setAttribute("src", num1);
+document.querySelector(".img2").setAttribute("src", num2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Win!"
+}else if (randomNumber1<randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Win!"
+}else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
